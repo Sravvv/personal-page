e7df1b15cb4a4c36c509313733a4158f5d58d82e
Sravvv <>
2021-09-30 22:57:25 -0600

    contact page, - github sidebar item

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -13,7 +13,6 @@ function Sidebar(props) {
                 <SidebarItem title="Projects" onClick={() => props.onChange('Projects')} active={props.current === 'Projects'}/>
                 <SidebarItem title="Resume" onClick={() => props.onChange('Resume')} active={props.current === 'Resume'}/>
                 <SidebarItem title="Contact" onClick={() => props.onChange('Contact')} active={props.current === 'Contact'}/>
-                <SidebarItem title="Github" onClick={() => window.location.href='https://github.com/Sravvv'}/>
             </Column>
         </Column>
     );  
